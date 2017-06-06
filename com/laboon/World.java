a22ded4860fceafa6de5b86206c99f7f338284f5
Jeff Krystek <jeffkrystek@wsp047387wss.wireless.pitt.edu>
2017-06-06 11:01:59 -0400

    changed

--- com/laboon/World.java
+++ com/laboon/World.java
@@ -7,15 +7,15 @@ public class World {
 	/**
 	 * Size of the world
 	 */
-	
+
 	int _size;
-	
+
 	/**
 	 * The world, a 2D array of Cells which can be living or dead.
 	 */
-	
+
 	Cell[][] _world;
-	
+
 	/**
 <<<<<<< HEAD
 	 * A random number generator (actually pseudo-random, of course).
@@ -23,48 +23,48 @@ public class World {
 	 * A pseudo-random number generator.
 >>>>>>> wjl_display
 	 */
-	
+
 	Random _rng;
-	
+
 	/**
 	 * Initial world constructor.
 	 * @param size Size of world
 	 * @param seed Random number generator seed
 	 * @param percent Initial percentage of cells alive
 	 */
-	
+
 	public World(int size, int seed, int percent) {
 		_size = size;
 		_rng = new Random(seed);
 		_world = generateBoard(size, percent);
 	}
-	
+
 	/**
 	 * Iterated (non-initial) world constructor.
          *
 	 * @param cells Cells living in the new world
 	 * @param rng Random number generator
 	 */
-	
+
 	public World(Cell[][] cells, Random rng) {
 		_size = cells.length;
 		_rng = rng;
 		_world = cells;
 	}
-	
+
 	/**
 	 * Generate the initial state of a cell, given the percentage
 	 * chance that it is living.
 	 * @param percent percentage chance that it is living
 	 * @return state of cell, State.ALIVE or State.DEAD
 	 */
-	
+
 	private State generateInitialState(int percent) {
 		int livingChance = _rng.nextInt(100);
 		State toReturn = (livingChance < percent) ? State.ALIVE : State.DEAD;
 		return toReturn;
 	}
-	
+
 	/**
 	 * The number of living neighbors that a cell has.
 	 * @param world the world
@@ -72,19 +72,19 @@ public class World {
 	 * @param y y location of cell
 	 * @return
 	 */
-	
+
 	private int getNumNeighbors(Cell[][] world, int x, int y) {
 		int size = world.length;
 		int leftX = (x - 1) % size;
 		int rightX = (x + 1) % size;
 		int upY = (y - 1) % size;
 		int downY = (y + 1) % size;
-		
+
 		if (leftX == -1) { leftX = size - 1; }
 		if (rightX == -1) { rightX = size - 1; }
 		if (upY == -1) { upY = size - 1; }
 		if (downY == -1) { downY = size - 1; }
-		
+
 		int numNeighbors = 0;
 
 		if (world[leftX][upY].isAlive())    { numNeighbors++; }
@@ -98,12 +98,12 @@ public class World {
 
 		return numNeighbors;
 	}
-	
+
 	/**
 	 * Go through one iteration of this World and return new World.
 	 * @return New world
 	 */
-	
+
 	public World iterate() {
 		Cell[][] newCells = new Cell[_size][_size];
 		for (int j = 0; j < _size; j++ ) {
@@ -118,13 +118,9 @@ public class World {
 	 * Convert this World to a string for display.
 	 * @return String representation of world
 	 */
-	
+
 	public String toString() {
-<<<<<<< HEAD
 		String toReturn = "> ";
-=======
-		String toReturn = "* ";
->>>>>>> wjl_display
 		for (int j= 0; j < _size; j++) {
 			toReturn += String.valueOf(j % 10);
 		}
@@ -138,14 +134,14 @@ public class World {
 		}
 		return toReturn;
 	}
-	
+
 	/**
 	 * Generate initial game board.
 	 * @param size Size of board
 	 * @param percent Percent alive
 	 * @return Initial world
 	 */
-	
+
 	private Cell[][] generateBoard(int size, int percent) {
 		Cell[][] world = new Cell[size][size];
 		for (int j = 0; j < size; j++ ) {
@@ -155,5 +151,5 @@ public class World {
 		}
 		return world;
 	}
-	
+
 }
